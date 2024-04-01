fab81dd6be89b1991b76addf763420768af35529
Clayton Cirqueira <cirqueira.info@gmail.com>
2024-04-01 17:25:22 -0300

    nova função para recarregar página

--- lamp.js
+++ lamp.js
@@ -1,37 +1,43 @@
 //variáveis de acesso id
-const turnOn = document.getElementById ( 'turnOn' );
-const turnOff = document.getElementById ( 'turnOff' );
-const lamp = document.getElementById ( 'lamp' );
-const turnBroken = document.getElementById( 'turnBroken');
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+const turnBroken = document.getElementById('turnBroken');
+const turnUpdating = document.getElementById('turnUpdating');
 
 //procura pela string 'quebrada no atributo src'
-function isLampBroken (){
-    return lamp.src.indexOf('quebrada')>-1
+function isLampBroken() {
+    return lamp.src.indexOf('quebrada') > -1
 }
 
 //se não estiver quebrada poderá ligar
-function lampOn (){    
-   if(!isLampBroken()){
-    lamp.src = './img/ligada.jpg';
-   }
+function lampOn() {
+    if (!isLampBroken()) {
+        lamp.src = './img/ligada.jpg';
+    }
 }
 
 //vou desligar apenas lâmpadas que não estiverem quebradas
-function lampOff (){    
-    if(!isLampBroken()){
+function lampOff() {
+    if (!isLampBroken()) {
         lamp.src = './img/desligada.jpg';
     }
 }
 
 //função que troca imagem por lâmpada quebrada
-function lampBroken (){
+function lampBroken() {
     lamp.src = './img/quebrada.jpg';
 }
 
+function reload() {
+    window.location.reload();
+}
+
 //eventos de clique e mouseover
-turnOn.addEventListener ( 'click', lampOn );
-turnOff.addEventListener ( 'click', lampOff );
-turnBroken.addEventListener ( 'click', lampBroken );
-lamp.addEventListener ( 'mouseover', lampOn );
-lamp.addEventListener ( 'mouseleave', lampOff );
-lamp.addEventListener ( 'dblclick', lampBroken );
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
+turnBroken.addEventListener('click', lampBroken);
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+lamp.addEventListener('dblclick', lampBroken);
+turnUpdating.addEventListener('click',reload);
