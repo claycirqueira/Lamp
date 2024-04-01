ec223d629e4db6b2ab3a278eeb23d0314e3de6d0
Clayton Cirqueira <cirqueira.info@gmail.com>
2024-04-01 16:51:39 -0300

    funções e eventos

--- lamp.js
+++ lamp.js
@@ -1,18 +1,35 @@
-const turnOn = document.getElementById( 'turnOn');
-const turnOff = document.getElementById( 'turnOff');
-const lamp = document.getElementById( 'lamp');
+//variáveis de acesso id
+const turnOn = document.getElementById ( 'turnOn' );
+const turnOff = document.getElementById ( 'turnOff' );
+const lamp = document.getElementById ( 'lamp' );
 
+//procura pela string 'quebrada no atributo src'
+function isLampBroken (){
+    return lamp.src.indexOf('quebrada')>-1
+}
+
+//se não estiver quebrada poderá ligar
 function lampOn (){    
+   if(!isLampBroken()){
     lamp.src = './img/ligada.jpg';
-}
-function lampOff (){    
-    lamp.src = './img/desligada.jpg';
+   }
 }
 
+//vou desligar apenas lâmpadas que não estiverem quebradas
+function lampOff (){    
+    if(!isLampBroken()){
+        lamp.src = './img/desligada.jpg';
+    }
+}
+
+//função que troca imagem por lâmpada quebrada
 function lampBroken (){
     lamp.src = './img/quebrada.jpg';
 }
 
-turnOn.addEventListener( 'click',lampOn);
-turnOff.addEventListener( 'click',lampOff);
-lamp.addEventListener( 'click',lampBroken);
+//eventos de clique e mouseover
+turnOn.addEventListener ( 'click', lampOn );
+turnOff.addEventListener ( 'click', lampOff );
+lamp.addEventListener ( 'mouseover', lampOn );
+lamp.addEventListener ( 'mouseleave', lampOff );
+lamp.addEventListener ( 'dblclick', lampBroken );
